101e44f754b6ce28592b239ca1d7a0559b3b698d
BayuRachmawan1321 <bayurahmawan1321@gmail.com>
2024-05-19 08:03:43 +0700

    Task Bayu Rahmawan 19 Mei 2024
    - Mengatur Hover List Navbar ketika List Active
    - Mengatur Logic ketika User Type Session nya "user" maka tombol tambah Loker dihilangkan (Karena User Type "User" seharusnya tidak dapat menambahkan Loker)

--- php/template/navbar.php
+++ php/template/navbar.php
@@ -1,10 +1,22 @@
+
+<?php 
+$uriArray = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uriSegments = explode("/", $uriArray); 
+?>
+
 <div class='navbar'>
     <h1>Selamat Datang <?php echo $sessionChecker->getEmail(); ?></h1>
     <div class="listPages">
-        <a href='index.php'>Home</a>
-        <a href='listLoker.php'>List Loker</a>
-        <a href='tambahLoker.php'>Tambah Loker</a>
-        <a href='tipsMencariPekerjaan.php'>Tips Mencari Pekerjaan</a>
-        <a href="./php/logic/Logout.php">Logout</a>
+        <a class="<?php if($uriSegments[2] == "index.php") echo "active" ?>" href='index.php'>Home</a>
+        <a class="<?php if($uriSegments[2] == "listLoker.php") echo "active" ?>" href='listLoker.php'>List Loker</a>
+        <?php 
+            if($_SESSION['user_type'] == "perusahaan" && $uriSegments[2] == "tambahLoker.php"){
+                echo "<a class='active' href='tambahLoker.php'>Tambah Loker</a>";
+            }else{
+                echo "<a class='' href='tambahLoker.php'>Tambah Loker</a>";
+            }
+        ?>
+        <a class="<?php if($uriSegments[2] == "tipsMencariPekerjaan.php") echo "active" ?>" href='tipsMencariPekerjaan.php'>Tips Mencari Pekerjaan</a>
+        <a class="" href="./php/logic/Logout.php">Logout</a>
     </div>
 </div>
